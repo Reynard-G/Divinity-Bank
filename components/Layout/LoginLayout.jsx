5655831667b43f5c91220e8f2aab357175d1de08
Reynard G <40525094+Reynard-G@users.noreply.github.com>
2024-03-27 03:45:40 +0000

    Update layout class names and database schema

--- components/Layout/LoginLayout.jsx
+++ components/Layout/LoginLayout.jsx
@@ -1,6 +1,6 @@
 export default function LoginLayout({ children }) {
   return (
-    <div className='relative flex min-h-dvh flex-col bg-transparent'>
+    <div className='relative flex min-h-svh flex-col bg-transparent'>
       <div className='flex h-screen items-center justify-center p-4'>
         <div className='flex h-full w-full items-center justify-center'>
           <div className='flex w-full max-w-sm flex-col gap-4'>{children}</div>
