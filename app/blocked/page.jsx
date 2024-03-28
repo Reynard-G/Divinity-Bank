9c92ee2e9b98d4123301f108e47dbefcf47862c2
Reynard G <40525094+Reynard-G@users.noreply.github.com>
2024-03-28 20:05:48 +0000

    Add animated to ratelimit/blocked page

--- app/blocked/page.jsx
+++ app/blocked/page.jsx
@@ -1,8 +1,8 @@
 export default function Blocked() {
   return (
-    <div className='flex flex-col items-center justify-center h-dvh gap-4'>
-      <h1 className='text-5xl font-bold animate-fade-down'>Woah!</h1>
-      <p className='text-xl text-center animate-fade-up animate-delay-200 animate-duration-750'>
+    <div className='flex h-dvh flex-col items-center justify-center gap-4'>
+      <h1 className='animate-fade-down text-5xl font-bold'>Woah!</h1>
+      <p className='animate-duration-750 animate-fade-up text-center text-xl animate-delay-200'>
         You&apos;re asking a lot from me, why not take a breather for a second?
       </p>
     </div>
