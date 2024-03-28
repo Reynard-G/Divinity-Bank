1c75b9433cc36129afa53974e63a57886f5d90f8
Reynard-G <reynard.ean.gunawan@gmail.com>
2024-03-27 23:36:26 -0500

    Fix non-escaped apostrophe

--- app/blocked/page.jsx
+++ app/blocked/page.jsx
@@ -3,7 +3,7 @@ export default function Blocked() {
     <div className='flex flex-col items-center justify-center h-dvh gap-4'>
       <h1 className='text-5xl font-bold animate-fade-down'>Woah!</h1>
       <p className='text-xl text-center animate-fade-up animate-delay-200 animate-duration-750'>
-        You're asking a lot from me, why not take a breather for a second?
+        You&apos;re asking a lot from me, why not take a breather for a second?
       </p>
     </div>
   );
