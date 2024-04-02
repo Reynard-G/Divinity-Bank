251ebbb16f3598f85a3be7f24f959313cf1d1018
Reynard-G <reynard.ean.gunawan@gmail.com>
2024-04-01 22:00:48 -0500

    Add Content-Security-Policy header to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,36 @@ const withBundleAnalyzer = NextBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' https://crafatar.com data:;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  connect-src 'self' https://*.ingest.sentry.io;
+  block-all-mixed-content;
+  upgrade-insecure-requests;
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ];
+  },
   experimental: {
     serverActions: {
       allowedOrigins: ['localhost:3000'],
