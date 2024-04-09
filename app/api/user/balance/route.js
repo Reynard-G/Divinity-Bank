884f6d8d8081284213f92325882c6ba12a6b5ea3
Reynard-G <reynard.ean.gunawan@gmail.com>
2024-04-08 22:24:47 -0500

    fix: Only include successful transactions in balance & subtract fees

--- app/api/user/balance/route.js
+++ app/api/user/balance/route.js
@@ -1,6 +1,7 @@
 import { cookies } from 'next/headers';
 
 import TransactionType from '@/constants/TransactionType';
+import TransactionStatus from '@/constants/TransactionStatus';
 import prisma from '@/lib/db';
 import getPayloadFromJWT from '@/utils/getPayloadFromJWT';
 
@@ -12,7 +13,7 @@ export async function GET() {
 
     // Using Promise.all to execute both queries concurrently
     // This is more efficient than executing them sequentially with await
-    const [creditSumResult, debitSumResult] = await Promise.all([
+    const [creditSumResult, debitSumResult, feeResult] = await Promise.all([
       prisma.transactions.aggregate({
         _sum: {
           amount: true,
@@ -20,6 +21,7 @@ export async function GET() {
         where: {
           user_id: id,
           transaction_type: TransactionType.CREDIT,
+          status: TransactionStatus.SUCCESS,
         },
       }),
       prisma.transactions.aggregate({
@@ -29,13 +31,24 @@ export async function GET() {
         where: {
           user_id: id,
           transaction_type: TransactionType.DEBIT,
+          status: TransactionStatus.SUCCESS,
+        },
+      }),
+      prisma.transactions.aggregate({
+        _sum: {
+          fee: true,
+        },
+        where: {
+          user_id: id,
+          status: TransactionStatus.SUCCESS,
         },
       }),
     ]);
 
     const creditSum = creditSumResult?._sum?.amount ?? 0.0;
     const debitSum = debitSumResult?._sum?.amount ?? 0.0;
-    const userBalance = creditSum.sub(debitSum).toString();
+    const feeSum = feeResult?._sum?.fee ?? 0.0;
+    const userBalance = creditSum.sub(debitSum).sub(feeSum).toNumber();
 
     return new Response(JSON.stringify({ userId: id, balance: userBalance }), {
       status: 200,
