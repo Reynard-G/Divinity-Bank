69e9b01f62fea05e8c40ee7ef102d31946a37a8e
Reynard-G <reynard.ean.gunawan@gmail.com>
2024-04-11 18:54:39 -0500

    build: Add unlighthouse.config.js file with site configuration and authentication hook

---
+++ unlighthouse.config.js
@@ -0,0 +1,26 @@
+export default {
+  site: 'divinity.milklegend.xyz',
+  cache: false,
+  debug: true,
+  chrome: {
+    useSystem: true,
+  },
+  scanner: {
+    device: 'desktop',
+    throttle: true,
+  },
+  hooks: {
+    async authenticate(page) {
+      // login to the page
+      await page.goto('https://divinity.milklegend.xyz/login');
+      const usernameInput = await page.$('input[type="text"]');
+      await usernameInput.type('null');
+      const passwordInput = await page.$('input[type="password"]');
+      await passwordInput.type('69UnL1ghth0u53');
+      await Promise.all([
+        page.click('button[type="submit"]'),
+        page.waitForNavigation(),
+      ]);
+    },
+  },
+};
