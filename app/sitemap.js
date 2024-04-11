876507258588335bad83676ef64614f274fde745
Reynard G <40525094+Reynard-G@users.noreply.github.com>
2024-04-11 23:12:00 +0000

    chore: Add new pages to sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -20,5 +20,35 @@ export default function sitemap() {
       changeFrequency: 'monthly',
       priority: 0.9,
     },
+    {
+      url: Page.BASE_URL + Page.BLOCKED,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.1,
+    },
+    {
+      url: Page.BASE_URL + Page.DASHBOARD,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.75,
+    },
+    {
+      url: Page.BASE_URL + Page.TRANSACTIONS,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.75,
+    },
+    {
+      url: Page.BASE_URL + Page.LOANS,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.75,
+    },
+    {
+      url: Page.BASE_URL + Page.SETTINGS,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.75,
+    },
   ];
 }
