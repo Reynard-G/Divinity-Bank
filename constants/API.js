20d0bc6fc3987e21c1112ee05da15bf15461d2ae
Reynard-G <reynard.ean.gunawan@gmail.com>
2024-04-14 00:50:18 -0500

    refactor: Remove transaction status routes

--- constants/API.js
+++ constants/API.js
@@ -1,9 +1,6 @@
 export default {
   USER: '/api/user',
   USER_TRANSACTIONS: '/api/user/transactions',
-  USER_TRANSACTIONS_STATUS_FAILED: '/api/user/transactions/status/failed',
-  USER_TRANSACTIONS_STATUS_PENDING: '/api/user/transactions/status/pending',
-  USER_TRANSACTIONS_STATUS_SUCCESS: '/api/user/transactions/status/success',
   USER_BALANCE: '/api/user/balance',
   USERS: '/api/users',
 };
