0b1b81a5142147b373aab11f06ecee3fddf2fa98
Reynard G <40525094+Reynard-G@users.noreply.github.com>
2024-04-26 15:27:18 +0000

    feat: Add image upload to R2 + view transaction attachments in individual transaction pages

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const cspHeader = `
   default-src 'self';
   script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.cloudflareinsights.com;
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-  img-src 'self' https://crafatar.com data: blob:;
+  img-src 'self' https://crafatar.com https://imgs.divinity.milklegend.xyz data: blob:;
   font-src 'self' https://fonts.gstatic.com;
   object-src 'none';
   worker-src 'self' blob:;
--- utils/base64FromFile.js
+++ utils/base64FromFile.js
@@ -1,10 +1,7 @@
 export default async function base64FromFile(file) {
   return new Promise((resolve, reject) => {
     const fileReader = new FileReader();
-    fileReader.onload = () => {
-      const base64 = fileReader.result.split(',')[1];
-      resolve(base64);
-    };
+    fileReader.onload = () => resolve(fileReader.result);
     fileReader.onerror = (error) => reject(error);
     fileReader.readAsDataURL(file);
   });
