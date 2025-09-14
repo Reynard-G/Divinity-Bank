14f7cbb2cac49e5b28dbfcc89140f82dc337b144
Reynard-G <reynard.ean.gunawan@gmail.com>
2025-09-14 01:13:43 -0500

    refactor: :zap: remove redundant check if servers are available as we throw error in `/app/server/[server]/layout.tsx`

--- src/app/app/server/[server]/servers/page.tsx
+++ src/app/app/server/[server]/servers/page.tsx
@@ -4,28 +4,18 @@ import { ServerDirectoryParams } from "@/app/app/server/[server]/layout";
 import { PageHeader } from "@/components/ui/page-header";
 import { ServerGrid } from "@/components/servers/server-grid";
 import { ServerGridSkeleton } from "@/components/servers/server-grid-skeleton";
-import { getServers } from "@/lib/db/queries/server.queries";
 import { SummaryCards } from "@/components/servers/summary-cards";
 import { SummaryCardsSkeleton } from "@/components/servers/summary-cards-skeleton";
 
 export default async function ServersPage({ params }: ServerDirectoryParams) {
   const { server: currentServerSlug } = await params;
 
-  const servers = await getServers();
-
   return (
     <div className="mx-auto flex w-full max-w-7xl grow flex-col">
-      {servers && servers.length > 0 ? (
-        <PageHeader
-          title="Servers"
-          description="View and manage your accounts across different servers."
-        />
-      ) : (
-        <PageHeader
-          title="Servers"
-          description="No servers available. Contact an administrator."
-        />
-      )}
+      <PageHeader
+        title="Servers"
+        description="View and manage your accounts across different servers."
+      />
 
       <Suspense fallback={<SummaryCardsSkeleton />}>
         <SummaryCards />
