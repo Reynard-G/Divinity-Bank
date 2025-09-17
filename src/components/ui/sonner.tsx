783bcec176838cb91d830fd031384679cb7ddc9f
Reynard-G <reynard.ean.gunawan@gmail.com>
2025-09-16 23:33:28 -0500

    style: :lipstick: enhance description class for Toaster component with font-light

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -12,7 +12,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
         classNames: {
           toast:
             "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-          description: "group-[.toast]:text-muted-foreground",
+          description:
+            "group-[.toast]:text-muted-foreground group-[.toast]:font-light",
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
           cancelButton:
