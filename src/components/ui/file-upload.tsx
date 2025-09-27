f5272c6ec54e240c83834aa81c321a1408cc5238
Reynard-G <reynard.ean.gunawan@gmail.com>
2025-09-27 01:21:54 -0500

    fix: :rotating_light: comment out aria attributes and adjust img element linting

--- src/components/ui/file-upload.tsx
+++ src/components/ui/file-upload.tsx
@@ -807,8 +807,8 @@ function FileUploadDropzone(props: FileUploadDropzoneProps) {
       role="region"
       id={context.dropzoneId}
       aria-controls={`${context.inputId} ${context.listId}`}
-      aria-disabled={context.disabled}
-      aria-invalid={invalid}
+      //aria-disabled={context.disabled}
+      //aria-invalid={invalid}
       data-disabled={context.disabled ? "" : undefined}
       data-dragging={dragOver ? "" : undefined}
       data-invalid={invalid ? "" : undefined}
@@ -892,7 +892,7 @@ function FileUploadList(props: FileUploadListProps) {
     <ListPrimitive
       role="list"
       id={context.listId}
-      aria-orientation={orientation}
+      //aria-orientation={orientation}
       data-orientation={orientation}
       data-slot="file-upload-list"
       data-state={shouldRender ? "active" : "inactive"}
@@ -1086,8 +1086,10 @@ function FileUploadItemPreview(props: FileUploadItemPreviewProps) {
         }
 
         return (
-          // biome-ignore lint/performance/noImgElement: dynamic file URLs from user uploads don't work well with Next.js Image optimization
-          <img src={url} alt={file.name} className="size-full object-cover" />
+          <>
+            {/* eslint-disable-next-line @next/next/no-img-element -- dynamic file URLs from user uploads don't work well with Next.js Image optimization */}
+            <img src={url} alt={file.name} className="size-full object-cover" />
+          </>
         );
       }
 
